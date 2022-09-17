716271d5f12fc77e36a203a3e841e04f955c19a0
Matthew Bennett <matthew.bennett@uclouvain.be>
2022-09-17 23:10:58 +0200

    Add custom r_config.R file and shadow alias R

---
+++ r_config.R
@@ -0,0 +1,6 @@
+# error when using if condition logical vector of greater than length 1
+Sys.setenv("_R_CHECK_LENGTH_1_CONDITION_" = "true")
+
+# when subsetting a data frame with df$var, tell me if var was not in there but
+# some other column partially matched (i.e. variance)
+options(warnPartialMatchDollar = TRUE)
