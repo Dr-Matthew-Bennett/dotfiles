7ee3906dd44b99de306c3d65e5c49408fa26ae00
Dr-Matthew-Bennett <bennettmatt4@gmail.com>
2025-05-13 09:23:43 +0100

    Slim down r snip

--- snips/function.r
+++ snips/function.r
@@ -1,20 +1,5 @@
-#' @title do this task
-#' 
-#' @description a longer description
-#' 
-#' @param x A number
-#' @param y A number
-#'
-#' @return A numeric vector
-#'
-#' @example
-#'
-#' @export
-#'
 function_name <- function(df) {
-  type = class(df) 
-  dt = data.table::copy(data.table::setDT(df))
 
-  return (f_restore_class(dt, type))
+  return ()
 }
 
